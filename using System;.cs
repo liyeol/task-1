ad4da36975d5140d8d856b2d7949657408e5b88b
liyeol <98913681+liyeol@users.noreply.github.com>
2022-10-21 14:59:55 +0100

    ver2

--- using System;.cs
+++ using System;.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args) => Console.WriteLine("Hello World!");
+        static void Main(string[] args) => Console.WriteLine("Hello!");
+
     }
 }
